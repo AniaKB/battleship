e2368a7aabdcee2a7bede85b05de3f3e033e0529
Bunny <plank@Bunnys-Mac-mini.attlocal.net>
2023-06-15 15:37:18 -0400

    adding html and js pages

---
+++ battleship.js
@@ -0,0 +1,22 @@
+let location1 = 3;
+let location2 = 4;
+let location3 = 5;
+
+let guess;
+let hits = 0;
+let guesses = 0;
+let isSunk = false;
+
+while (isSunk === false) {
+  // get user's guess
+  // compare input to values
+  if (guess === false) {
+    //tell user to enter number
+  } else {
+    // add 1 to guesses
+    if (/* # hits === 3 */) {
+      isSunk === true
+      console.log("You sunk my battleship!")
+      } 
+    }
+  }
